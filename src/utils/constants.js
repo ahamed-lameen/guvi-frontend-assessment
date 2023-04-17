86ece9a9ab8e4678289359367665631baabdd455
ahamed lameen <ahamedlameen@gmail.com>
2023-04-17 06:26:01 +0530

    guvi-frontend-assessment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,3 @@
-export const LOGIN_API = "http://localhost:8080/login";
-export const CREATE_ACCOUNT_API = "http://localhost:8080/createAccount";
-export const SAVE_MY_PROFILE_API = "http://localhost:8080/saveMyProfile";
-// const url = 'https://guvi-backend.herokuapp.com/login';
-// const url = 'https://guvi-backend.herokuapp.com/createAccount';
-// const url = 'https://guvi-backend.herokuapp.com/saveMyProfile';
+export const LOGIN_API = "https://guvi-backend.herokuapp.com/login";
+export const CREATE_ACCOUNT_API = "https://guvi-backend.herokuapp.com/createAccount";
+export const SAVE_MY_PROFILE_API = "https://guvi-backend.herokuapp.com/saveMyProfile";
